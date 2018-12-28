a3c0532010c94b8220cce5d878a8a32c33ca9cc7
Ash Lux <alux@zealcon.com>
2018-12-27 23:10:05 -0600

    Refactor

--- find-unused-steps.js
+++ find-unused-steps.js
@@ -7,12 +7,7 @@ function startTrackingUsages(cucumberRuntime) {
         return _.wrap(funcToMonkeyPatch, function (givenFn, matcher, stepFn) {
             const that = this;
             usages[matcher] = 0;
-            const stepFnWrapped = _.wrap(stepFn, function () {
-                const that = this;
-                usages[matcher] = usages[matcher] + 1;
-                return stepFn.apply(that, arguments);
-            });
-            return givenFn.call(that, matcher, stepFnWrapped);
+            return givenFn.call(that, matcher, stepFn);
         });
     }
 
@@ -21,22 +16,28 @@ function startTrackingUsages(cucumberRuntime) {
     cucumberRuntime.Then = monkeyPatch(cucumberRuntime.Then);
 }
 
+function calculateUsages(features) {
+    _.forEach(features, function (feature) {
+        _.forEach(feature.getScenarios(), function (scenario) {
+            _.forEach(scenario.getSteps(), function (step) {
+                _.forEach(_.keys(usages), function (matcher) {
+                    if (new RegExp(matcher.substring(1, matcher.length - 1)).test(step.getName())) {
+                        usages[matcher] = usages[matcher] + 1;
+                    }
+                });
+            });
+        });
+    });
+
+    return usages;
+}
+
 module.exports = {
     findUnusedSteps: function (cucumberRuntime) {
         startTrackingUsages(cucumberRuntime);
 
         cucumberRuntime.registerHandler('AfterFeatures', function (features, callback) {
-            _.forEach(features, function (feature) {
-                _.forEach(feature.getScenarios(), function (scenario) {
-                    _.forEach(scenario.getSteps(), function (step) {
-                        _.forEach(_.keys(usages), function (matcher) {
-                            if (new RegExp(matcher.substring(1, matcher.length - 1)).test(step.getName())) {
-                                usages[matcher] = usages[matcher] + 1;
-                            }
-                        });
-                    });
-                });
-            });
+            calculateUsages(features);
 
             const unusedSteps = _.keys(_.pickBy(usages, function (numberOfUsages) {
                 return numberOfUsages === 0;
